09b3fefe5d6e3cb95b2c1da634f58225b2d15f9b
Yazhini S <yazhinis023@gmail.com>
2025-07-17 14:43:08 +0530

    Updation7

--- cos/src/Components/AdminPanel.jsx
+++ cos/src/Components/AdminPanel.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import './css/AdminPanel.css'
 const AdminPanel = () => {
   return (
-    <div>
-      Admin panel
+    <div className='addProduct'>
+      <div className='addProducts-form'>
+        <h2>Add Product</h2>
+        <label htmlFor='productname'>Product Name:</label>
+        <input type='text' required/>
+
+        <label htmlFor='price'>Price:</label>
+        <input type='number' required/>
+
+        <label htmlFor='description'>Description:</label>
+        <textarea required></textarea>
+        
+        <label htmlFor='image-url'>Image URL:</label>
+        <input type='text' required/>
+
+        <button>Add the Products</button>
+      </div>
     </div>
   )
 }
